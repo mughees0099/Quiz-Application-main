f62148b0e3dc442ed8c476b56da098468efe929e
Mughees javed <mugheesk80@gmail.com>
2024-10-14 18:13:44 +0500

    changed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const resportsRoute = require("./routes/reportsRoute");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -14,6 +16,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
@@ -21,6 +25,7 @@ app.use("/api/reports", resportsRoute);
 const port = process.env.PORT || 5000;
 
 const path = require("path");
+const bodyParser = require("body-parser");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
